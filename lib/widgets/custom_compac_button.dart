d522344b0ffb07c8fa950c13cb32300b6f187e20
himanshuyadav146 <himanshuyadav146@gmail.com>
2022-11-16 08:00:10 +0530

    Added new graph and remove old one

--- lib/widgets/custom_compac_button.dart
+++ lib/widgets/custom_compac_button.dart
@@ -13,26 +13,39 @@ class CustomCompacButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-      style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all(Color.fromRGBO(r, g, b, opacity)),
-        overlayColor: MaterialStateProperty.all(Color.fromRGBO(59, 89, 161, 1)),
-        side: MaterialStateProperty.all(BorderSide(
-            color: Colors.blue,
-            width: 1.0,
-            style: BorderStyle.solid
-        ))
+    return Container(
+      width: 108,
+      height: 35.29620361328125,
+      decoration: BoxDecoration(
+        borderRadius : BorderRadius.only(
+          topLeft: Radius.circular(8),
+          topRight: Radius.circular(8),
+          bottomLeft: Radius.circular(8),
+          bottomRight: Radius.circular(8),
+        ),
+        color : Color.fromRGBO(59, 89, 161, 1),
       ),
-        onPressed: onButtonClick, child: Text(text,
-        style: TextStyle(
-            color: Color.fromRGBO(txtR,txtG,txtB,txtOpacity),
-            fontFamily: 'RobotoRegular',
-            fontSize: 14,
-            letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-            fontWeight: FontWeight.normal,
-            height: 1.5 /*PERCENT not supported*/
-        )
-    )
+      child: TextButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Color.fromRGBO(r, g, b, opacity)),
+          overlayColor: MaterialStateProperty.all(Color.fromRGBO(59, 89, 161, 1)),
+          side: MaterialStateProperty.all(BorderSide(
+              color: Colors.blue,
+              width: 1.0,
+              style: BorderStyle.solid
+          ))
+        ),
+          onPressed: onButtonClick, child: Text(text,
+          style: TextStyle(
+              color: Color.fromRGBO(txtR,txtG,txtB,txtOpacity),
+              fontFamily: 'RobotoBold',
+              fontSize: 14,
+              letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+              fontWeight: FontWeight.normal,
+              height: 1.5 /*PERCENT not supported*/
+          )
+      )
+      ),
     );
   }
 }
